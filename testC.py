9e5f539931f46ccf65c859bf076e754a5d9e552d
José Guilherme Mont. <jguih@live.com>
2025-02-22 04:39:37 -0300

    Add files via upload

--- testC.py
+++ testC.py
@@ -1,124 +1,124 @@
 import streamlit as st
-import pandas as pd
-import yfinance as yf
-import seaborn as sns
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+from bcb import sgs
+
+@st.cache_data
+def get_data():
+    selic = sgs.get({'Selic': 432}, start='2000-01-01')
+    selic_atual = selic.iloc[-1].values[0]
+    ipca = sgs.get({'IPCA': 13522}, start='2000-01-01')
+    ipca_atual = ipca.iloc[-1].values[0]
+    
+    # Calculando juros reais
+    juros_reais = selic - ipca
+    juros_reais_atual = juros_reais.iloc[-1].values[0]
+    
+    return selic, selic_atual, ipca, ipca_atual, juros_reais, juros_reais_atual
 
 def app():
-    st.write('Análise Histórica')
-    st.subheader('Retorno Mensal')
-   
-import streamlit as st
+    st.title("Estatística Monetária")
 
+    # Obtendo dados com cache
+    selic, selic_atual, ipca, ipca_atual, juros_reais, juros_reais_atual = get_data()
 
-# Formulário principal
-with st.form(key='form_ativo'):
-    opcao = st.radio('Selecione:', ['Índices', 'Ações', 'Commodities'])
+    col1, col2 = st.columns([5, 1])
+    with col1:
+        # Criando gráfico interativo da Selic
+        fig_selic = go.Figure()
+        fig_selic.add_trace(go.Scatter(x=selic.index, y=selic['Selic'], mode='lines'))
+        fig_selic.add_trace(go.Scatter(x=[selic.index[-1]], y=[selic_atual], mode='markers', marker=dict(color='red', size=5)))
 
-    if opcao == 'Índices':
-        indices = {'IBOV': '^BVSP',
-                   'S&P500': '^GSPC',     
-                   'NASDAQ': '^IXIC',
-                   'FTSE100':'^FTSE',
-                   'DAX':'^GDAXI',
-                   'CAC40':'^FCHI',
-                   'SSE Composite':'000001.SS',
-                   'Nikkei225':'^N225',
-                   'Merval':'^MERV'}
+        fig_selic.update_layout(
+            title='Taxa de Juros SELIC',
+            title_x=0.4, 
+            yaxis_title='Taxa de Juros (%)',
+            showlegend=False,
+            plot_bgcolor='rgba(211, 211, 211, 0.15)'
+        )
+        fig_selic.update_yaxes(showgrid=True, gridwidth=0.1, gridcolor='gray', griddash='dot', zeroline=False, range=[0, fig_selic.data[0]['y'].max() * 1.1])
+        fig_selic.update_xaxes(showgrid=False, zeroline=False)
+
+        fig_selic.add_annotation(
+            x=selic.index[-1], 
+            y=selic_atual,
+            text=f'{selic_atual:.2f}%',
+            showarrow=True,
+            arrowhead=0,
+            ax=20,
+            ay=-40,
+            bordercolor='yellow'
+        )
+
+        # Criando gráfico interativo do IPCA
+        fig_ipca = go.Figure()
+        fig_ipca.add_trace(go.Scatter(x=ipca.index, y=ipca['IPCA'], mode='lines'))
+        fig_ipca.add_trace(go.Scatter(x=[ipca.index[-1]], y=[ipca_atual], mode='markers', marker=dict(color='red', size=5)))
+
+        fig_ipca.update_layout(
+            title='IPCA Acumulado 12M',
+            title_x=0.4, 
+            yaxis_title='IPCA acumulado (%)',
+            xaxis_title='Data',
+            showlegend=False,
+            plot_bgcolor='rgba(211, 211, 211, 0.15)'
+        )
+        fig_ipca.update_yaxes(showgrid=True, gridwidth=0.1, gridcolor='gray', griddash='dot', zeroline=False, range=[0, fig_ipca.data[0]['y'].max() * 1.1])
+        fig_ipca.update_xaxes(showgrid=False, zeroline=False)
+
+        fig_ipca.add_annotation(
+            x=ipca.index[-1], 
+            y=ipca_atual,
+            text=f'{ipca_atual:.2f}%',
+            showarrow=True,
+            arrowhead=0,
+            ax=20,
+            ay=-40,
+            bordercolor='yellow'
+        )
+
+        # Criando gráfico do Juro Real
+        fig_juros_reais = go.Figure()
+        fig_juros_reais.add_trace(go.Scatter(x=juros_reais.index, y=juros_reais.iloc[:, 0], mode='lines'))
+        fig_juros_reais.add_trace(go.Scatter(x=[juros_reais.index[-1]], y=[juros_reais_atual], mode='markers', marker=dict(color='red', size=5)))
+
+        fig_juros_reais.update_layout(
+            title='Juro Real (SELIC - IPCA)',
+            title_x=0.4, 
+            yaxis_title='Juro Real (%)',
+            xaxis_title='Data',
+            showlegend=False,
+            plot_bgcolor='rgba(211, 211, 211, 0.15)'
+        )
+        fig_juros_reais.update_yaxes(showgrid=True, gridwidth=0.1, gridcolor='gray', griddash='dot', zeroline=False, range=[juros_reais.iloc[:, 0].min() * 1.1, juros_reais.iloc[:, 0].max() * 1.1])
+        fig_juros_reais.update_xaxes(showgrid=False, zeroline=False)
+
+        fig_juros_reais.add_annotation(
+            x=juros_reais.index[-1], 
+            y=juros_reais_atual,
+            text=f'{juros_reais_atual:.2f}%',
+            showarrow=True,
+            arrowhead=0,
+            ax=20,
+            ay=-40,
+            bordercolor='yellow'
+        )
+
+        # Exibindo os gráficos no Streamlit
+        st.plotly_chart(fig_selic)
+        st.plotly_chart(fig_ipca)
+        st.plotly_chart(fig_juros_reais)
+
+    with col2:
+        st.write('')
+        st.write('')
+
+        iframe_code = """
+        <div style="text-align: center; padding: 10px; font-family: sans-serif;">
+            <span style="font-size: 16px; font-weight: bold; display: block; margin-bottom: 8px; color: white;">Mundo</span>
+            <iframe frameborder="0" scrolling="no" height="146" width="108" allowtransparency="true" marginwidth="0" marginheight="0" 
+            src="https://sslirates.investing.com/index.php?rows=1&bg1=FFFFFF&bg2=F1F5F8&text_color=333333&enable_border=hide&border_color=0452A1&
+            header_bg=ffffff&header_text=FFFFFF&force_lang=12" align="center"></iframe>
+        </div>
+        """
         
-        escolha = st.selectbox('Selecione o Índice', list(indices.keys()))
-        analisar = st.form_submit_button('Analisar')
-        ticker = indices[escolha]
-
-    elif opcao == 'Commodities':
-        commodities = {'Ouro': 'GC=F',
-                       'Prata': 'SI=F',
-                       'Platinum': 'PL=F',     
-                       'Cobre': 'HG=F',
-                       'WTI Oil':'CL=F',
-                       'Brent Oil':'BZ=F',
-                       'Milho':'ZC=F',
-                       'Soja':'ZS=F',
-                       'Café':'KC=F'}    
-        
-        escolha = st.selectbox('Selecione a Commodity', list(commodities.keys()))
-        analisar = st.form_submit_button('Analisar')
-        ticker = commodities[escolha]
-
-    elif opcao == 'Ações':
-        acoes = ['ALOS3', 'ABEV3', 'ASAI3', 'AURE3', 'AMOB3', 'AZUL4', 'AZZA3', 'B3SA3', 'BBSE3', 'BBDC3', 'BBDC4', 
-                 'BRAP4', 'BBAS3', 'BRKM5', 'BRAV3', 'BRFS3', 'BPAC11', 'CXSE3', 'CRFB3', 'CCRO3', 'CMIG4', 'COGN3', 
-                 'CPLE6', 'CSAN3', 'CPFE3', 'CMIN3', 'CVCB3', 'CYRE3', 'ELET3', 'ELET6', 'EMBR3', 'ENGI11', 'ENEV3', 
-                 'EGIE3', 'EQTL3', 'FLRY3', 'GGBR4', 'GOAU4', 'NTCO3', 'HAPV3', 'HYPE3', 'IGTI11', 'IRBR3', 'ISAE4', 
-                 'ITSA4', 'ITUB4', 'JBSS3', 'KLBN11', 'RENT3', 'LREN3', 'LWSA3', 'MGLU3', 'POMO4', 'MRFG3', 'BEEF3', 
-                 'MRVE3', 'MULT3', 'PCAR3', 'PETR3', 'PETR4', 'RECV3', 'PRIO3', 'PETZ3', 'PSSA3', 'RADL3', 'RAIZ4', 
-                 'RDOR3', 'RAIL3', 'SBSP3', 'SANB11', 'STBP3', 'SMTO3', 'CSNA3', 'SLCE3', 'SUZB3', 'TAEE11', 'VIVT3', 
-                 'TIMS3', 'TOTS3', 'UGPA3', 'USIM5', 'VALE3', 'VAMO3', 'VBBR3', 'VIVA3', 'WEGE3', 'YDUQ3']
-
-        # Criando um dicionário com chave como o nome da ação e valor como o nome da ação com '.SA'
-        acoes_dict = {acao: acao + '.SA' for acao in acoes}
-
-        escolha = st.selectbox('Selecione a Ação', list(acoes_dict.keys()))
-        analisar = st.form_submit_button('Analisar')
-        ticker = acoes_dict[escolha]
-        if analisar:
-            st.write(f'Você selecionou a ação: {escolha}')
-            st.write(f'Ticker: {ticker}')
-
-    if analisar:
-        data_inicial = ('1999-12-01')
-        data_final = ('2030-12-31')
-
-        # Baixa os dados do Yahoo Finance
-        dados = yf.download(ticker, start=data_inicial, end=data_final, interval="1mo")
-
-        if not dados.empty:
-            retornos = dados['Close'].pct_change().dropna()
-            # Adiciona colunas de ano e mês para organização
-            retornos = retornos.reset_index()
-            retornos['Year'] = retornos['Date'].dt.year
-            retornos['Month'] = retornos['Date'].dt.month
-
-            # Criar a tabela pivot sem média, apenas reorganizando os dados
-            tabela_retornos = retornos.pivot(index='Year', columns='Month', values=ticker)
-            tabela_retornos.columns = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 
-                                        'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
-
-            # Criando Heatmap
-            fig, ax = plt.subplots(figsize=(12, 9))
-            cmap = sns.color_palette('RdYlGn', 15)
-            sns.heatmap(tabela_retornos, cmap=cmap, annot=True, fmt='.2%', center=0, vmax=0.025, vmin=-0.025, cbar=False,
-                        linewidths=0.5, xticklabels=True, yticklabels=True, ax=ax)
-            ax.set_title(f'Heatmap Retorno Mensal - {escolha}', fontsize=18)
-            ax.set_yticklabels(ax.get_yticklabels(), rotation=0, verticalalignment='center', fontsize='12')
-            ax.set_xticklabels(ax.get_xticklabels(), fontsize='12')
-            plt.ylabel('')
-            st.pyplot(fig)
-
-            # Estatísticas
-            stats = pd.DataFrame(tabela_retornos.mean(), columns=['Média'])
-            stats['Mediana'] = tabela_retornos.median()
-            stats['Maior'] = tabela_retornos.max()
-            stats['Menor'] = tabela_retornos.min()
-            stats['Positivos'] = tabela_retornos.gt(0).sum() / tabela_retornos.count() # .gt(greater than) = Contagem de números maior que zero
-            stats['Negativos'] = tabela_retornos.le(0).sum() / tabela_retornos.count() # .le(less than) = Contagem de números menor que zero
-
-            # Stats_A
-            stats_a = stats[['Média', 'Mediana', 'Maior', 'Menor']].transpose()
-
-            fig, ax = plt.subplots(figsize=(12, 2))
-            sns.heatmap(stats_a, cmap=cmap, annot=True, fmt='.2%', center=0, vmax=0.025, vmin=-0.025, cbar=False,
-                        linewidths=0.5, xticklabels=True, yticklabels=True, ax=ax)
-            st.pyplot(fig)
-
-            # Stats_B
-            stats_b = stats[['Positivos', 'Negativos']].transpose()
-
-            fig, ax = plt.subplots(figsize=(12, 1))
-            sns.heatmap(stats_b, cmap=sns.color_palette("magma", as_cmap=True), annot=True, fmt='.2%', center=0, vmax=0.025, vmin=-0.025, cbar=False,
-                        linewidths=0.5, xticklabels=True, yticklabels=True, ax=ax)
-            st.pyplot(fig)
-
-        else:
-            st.error("Erro ao buscar os dados. Verifique o ticker ou tente novamente mais tarde.")
+        st.components.v1.html(iframe_code, height=180)
